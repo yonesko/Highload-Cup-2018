e85dccc23f9ac74ecd46ad4c9f55880a45e0a96d
Gleb Danichev <gbdanichev@avito.ru>
2020-06-04 14:43:18 +0300

    push

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-docker build -t dumb .
-docker tag dumb stor.highloadcup.ru/accounts/utopian_lizard
+docker build -t accounts .
+docker tag accounts stor.highloadcup.ru/accounts/utopian_lizard
 docker push stor.highloadcup.ru/accounts/utopian_lizard
